3ef82a52204e71ff1f88c7e56dc7d7d2a8ad1652
Viktor Gustavsson <villor94@gmail.com>
2014-08-07 22:55:18 +0200

    Added resize-handling

--- src/main.c
+++ src/main.c
@@ -1,12 +1,51 @@
 #include <ncurses.h>
+#include <stdlib.h>
+#include <signal.h>
+
+void handle_resize();
+void resize_signal();
+
+int wresized = 0;
+int busy = 0;
 
 int main(int argc, char const *argv[])
 {
+	int ch;
+
+	signal(SIGWINCH, resize_signal);
+
 	initscr();
-	printw("Kodapa!");
-	refresh();
-	getch();
+	cbreak();
+	keypad(stdscr, TRUE);
+	noecho();
+
+	while(1) {
+		if (wresized)
+			handle_resize();
+
+		ch = getch();
+		busy = 1;
+		addch(ch);
+		refresh();
+		busy = 0;
+	}
+
 	endwin();
 
 	return 0;
+}
+
+void handle_resize()
+{
+	addch('r');
+	refresh();
+	wresized = 0;
+}
+
+void resize_signal()
+{
+	if(busy)
+		wresized = 1;
+	else
+		handle_resize();
 }
