a9f72e2475d377768db712f8667c89e63dbbd863
Viktor Gustavsson <villor94@gmail.com>
2014-08-07 20:53:56 +0200

    Added ncurses

--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <ncurses.h>
 
 int main(int argc, char const *argv[])
 {
+	initscr();			/* Initialize curses 				*/
+	printw("Kodapa!");	/* Print 'Kodapa!' to the screen 	*/
+	refresh();			/* Print it to the actual screen	*/
+	getch();			/* Wait for user input				*/
+	endwin();			/* End curses mode					*/
+
 	return 0;
 }
