c8d9a67ec22b1ad41e04ec0181bf88b9d0ab85b7
Viktor Gustavsson <villor94@gmail.com>
2014-08-08 00:18:41 +0200

    Added timestamps to log.

--- src/log.c
+++ src/log.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include <time.h>
 
 #include "log.h"
 
 #define K_LOG_FP "kodapa.log"
+
+void fputs_timedate(FILE *fp)
+{
+	char s[25];
+	time_t t;
+	t = time(NULL);
+	struct tm *p = localtime(&t);
+	strftime(s, 25, "%c", p);
+	fputs(s, fp);
+}
 
 void klog(const char *fmt, ...) {
 	va_list arg;
@@ -12,9 +23,10 @@ void klog(const char *fmt, ...) {
 	FILE *fp;
 	fp = fopen(K_LOG_FP, "a+");
 
-	fprintf(fp, "LOG: ");
+	fputs_timedate(fp);
+	fputs(": ", fp);
 	vfprintf(fp, fmt, arg);
-	fprintf(fp, "\n");
+	fputs("\n", fp);
 
 	fclose(fp);
 	va_end(arg);
